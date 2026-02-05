93ec3bf907c846af5085e1efc6b8d5047fb504fc
AlfredoRosales12 <rosales.alfredo.goo@gmail.com>
2026-02-04 18:27:31 -0600

    Me equivoqué y coloqué una llave faltante XD

--- src/main/java/com/Proyectos/Inventario/Repository/Clientes.java
+++ src/main/java/com/Proyectos/Inventario/Repository/Clientes.java
@@ -1,6 +1,8 @@
 package com.Proyectos.Inventario.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import models.Cliente;
+
 
 public interface Clientes extends JpaRepository<Cliente, Long> {
 
--- src/main/java/models/Cliente.java
+++ src/main/java/models/Cliente.java
@@ -1,9 +1,9 @@
 package models;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 
 @Entity
-@Table(name="clientes")
+@Table(name = "clientes")
 public class Cliente {
 
     @Id
@@ -17,67 +17,62 @@ public class Cliente {
     private String estado;
     private String rfc;
 
+    public Cliente() {}
 
-    public Cliente(){}
-    
-    public Cliente(DatosCliente datosCliente){
-        this.nombre=nombre();
-        this.direccion=direccion();
-        this.municipio=municipio();
-        this.estado=estado();
-        this.rfc=rfc();
-    }  
+    public Cliente(DatosCliente datosCliente) {
+        this.nombre = nombre();
+        this.direccion = direccion();
+        this.municipio = municipio();
+        this.estado = estado();
+        this.rfc = rfc();
+    }
 
-public int getIdCliente() {
-    return idCliente;
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getMunicipio() {
+        return municipio;
+    }
+
+    public void setMunicipio(String municipio) {
+        this.municipio = municipio;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getRfc() {
+        return rfc;
+    }
+
+    public void setRfc(String rfc) {
+        this.rfc = rfc;
+    }
 }
 
-public void setIdCliente(int idCliente) {
-    this.idCliente = idCliente;
-
-public String getNombre() {
-    return nombre;
-}
-
-public void setNombre(String nombre) {
-    this.nombre = nombre;
-}
-
-public String getDireccion() {
-    return direccion;
-}
-
-public void setDireccion(String direccion) {
-    this.direccion = direccion;
-}
-
-public String getMunicipio() {
-    return municipio;
-}
-
-public void setMunicipio(String municipio) {
-    this.municipio = municipio;
-}
-
-public String getEstado() {
-    return estado;
-}
-
-public void setEstado(String estado) {
-    this.estado = estado;
-}
-
-public String getRfc() {
-    return rfc;
-}
-
-public void setRfc(String rfc) {
-    this.rfc = rfc;
-}
-
-
-
-
-
-
-}
