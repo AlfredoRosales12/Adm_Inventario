461749836b3bb421a9d5af261262a03ba7f72fac
AlfredoRosales12 <rosales.alfredo.goo@gmail.com>
2026-02-04 18:17:26 -0600

    Se agregó las variables de la clase Cliente con sus respectivos Getters and Setters

---
+++ src/main/java/com/Proyectos/Inventario/Repository/Clientes.java
@@ -0,0 +1,9 @@
+package com.Proyectos.Inventario.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface Clientes extends JpaRepository<Cliente, Long> {
+
+
+    
+}
